f2a5b0326092bbec1ca652e0194304ac3f32ed60
Alizzabeth <Alizzabeth@10.37.33.35>
2016-06-15 17:17:49 +0700

    Create Course Concrete Class

---
+++ University/ejbModule/univesity/calculation/Course.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package univesity.calculation;
+
+import university.course.Subject;
+
+/**
+ * @author Alizzabeth
+ *
+ */
+public class Course implements Subject{
+
+	@Override
+	public void setSubjectId(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getSubjectId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSubjectName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getSubjectName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCredit(int credit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getCredit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
