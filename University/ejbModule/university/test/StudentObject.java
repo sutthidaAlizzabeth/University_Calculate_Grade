c3e9bef9448051e6aa6e3b1f9fa8955d12e4b472
Alizzabeth <Alizzabeth@sutthidas-MacBook-Pro.local>
2016-06-16 09:24:05 +0700

    create JUnit test method

--- University/ejbModule/university/test/StudentObject.java
+++ University/ejbModule/university/test/StudentObject.java
@@ -7,114 +7,85 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import univesity.calculation.Student;
+
 /**
  * @author Alizzabeth
  *
  */
 public class StudentObject {
 
+	Student st = new Student();
+	String id = "56130500076";
+	String fname = "Sutthida";
+	String lname = "Chonpanichakul";
+	String dept = "SIT";
+	String year = "3";
+	
 	/**
 	 * Test method for {@link univesity.calculation.Student#setId(java.lang.String)}.
-	 */
-	@Test
-	public void testSetId() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link univesity.calculation.Student#getId()}.
 	 */
 	@Test
-	public void testGetId() {
-		fail("Not yet implemented");
+	public void testId() {
+		st.setId(id);
+		assertEquals(id, st.getId());
 	}
 
 	/**
 	 * Test method for {@link univesity.calculation.Student#setFname(java.lang.String)}.
-	 */
-	@Test
-	public void testSetFname() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link univesity.calculation.Student#getFname()}.
 	 */
 	@Test
-	public void testGetFname() {
-		fail("Not yet implemented");
+	public void testFname() {
+		st.setFname(fname);
+		assertEquals(fname, st.getFname());
 	}
+
 
 	/**
 	 * Test method for {@link univesity.calculation.Student#setLname(java.lang.String)}.
-	 */
-	@Test
-	public void testSetLname() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link univesity.calculation.Student#getLname()}.
 	 */
 	@Test
-	public void testGetLname() {
-		fail("Not yet implemented");
+	public void testLname() {
+		st.setLname(lname);
+		assertEquals(lname, st.getLname());
 	}
+
 
 	/**
 	 * Test method for {@link univesity.calculation.Student#setFullName(java.lang.String, java.lang.String)}.
-	 */
-	@Test
-	public void testSetFullName() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link univesity.calculation.Student#getFullName()}.
 	 */
 	@Test
-	public void testGetFullName() {
-		fail("Not yet implemented");
+	public void testFullName() {
+		st.setFullName(fname, lname);
+		assertEquals(fname + " " + lname, st.getFullName());
 	}
+
 
 	/**
 	 * Test method for {@link univesity.calculation.Student#setDept(java.lang.String)}.
-	 */
-	@Test
-	public void testSetDept() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link univesity.calculation.Student#getDept()}.
 	 */
 	@Test
-	public void testGetDept() {
-		fail("Not yet implemented");
+	public void testDept() {
+		st.setDept(dept);
+		assertEquals(dept, st.getDept());
 	}
+
 
 	/**
 	 * Test method for {@link univesity.calculation.Student#setYear(java.lang.String)}.
-	 */
-	@Test
-	public void testSetYear() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link univesity.calculation.Student#getYear()}.
 	 */
 	@Test
-	public void testGetYear() {
-		fail("Not yet implemented");
+	public void testYear() {
+		st.setYear(year);
+		assertEquals(year, st.getYear());
 	}
 
-	/**
-	 * Test method for {@link java.lang.Object#Object()}.
-	 */
-	@Test
-	public void testObject() {
-		fail("Not yet implemented");
-	}
+
 
 }
